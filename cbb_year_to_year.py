578ba14749245a15ff770b78918bbf479c65a37e
Jordan Siff <jordansiff@Jordans-MacBook-Pro.local>
2018-07-31 21:56:27 -0400

    add year to year score differentials

--- cbb_year_to_year.py
+++ cbb_year_to_year.py
@@ -12,6 +12,8 @@ pointsPerRound["National Championship"] = 65
 
 teamWinScores = {}
 
+teamScoreDiff = {}
+
 
 with open('NCAA_MM_Results.csv', 'rb') as csvfile:
 	reader = csv.reader(csvfile)
@@ -27,18 +29,23 @@ with open('NCAA_MM_Results.csv', 'rb') as csvfile:
 		rd = line[1]
 		winner = line[4]
 		loser = line[7]
-		#update team scores
+		scoreDiff = int(line[5]) - int(line[8])
+		#update team scores and differentials
 		if (winner,year) in teamWinScores.keys():
 			teamWinScores[(winner,year)] += pointsPerRound[rd]
+			teamScoreDiff[(winner,year)] += scoreDiff
 		else:
 			teamWinScores[(winner,year)] = pointsPerRound[rd]
+			teamScoreDiff[(winner,year)] = scoreDiff
 		if (loser,year) in teamWinScores.keys():
 			teamWinScores[(loser,year)] += 1
+			teamScoreDiff[(loser,year)] -= scoreDiff
 		else:
 			teamWinScores[(loser,year)] = 1
-
+			teamScoreDiff[(loser,year)] = scoreDiff * -1
 
 yearToYear = {}
+yearToYearDiff = {}
 
 for team in teamWinScores.keys():
 	school = team[0]
@@ -46,6 +53,7 @@ for team in teamWinScores.keys():
 	prevYear = year-1
 	try:
 		yearToYear[team] = (teamWinScores[(school,prevYear)],teamWinScores[team])
+		yearToYearDiff[team] = (teamScoreDiff[(school,prevYear)],teamScoreDiff[team])
 	except:
 		pass
 
@@ -53,11 +61,13 @@ for team in teamWinScores.keys():
 
 with open('cbb_year_to_year.csv', 'wb') as csvwrite:
 	writer = csv.writer(csvwrite)
-	writer.writerow(["school", "season", "prev winScore", "winScore"])
+	writer.writerow(["school", "season", "prev winScore", "winScore", "prev scoreDiff", "scoreDiff"])
 	for team in yearToYear.keys():
 		school = team[0]
 		year = team[1]
 		prevWinScore = yearToYear[team][0]
 		winScore = yearToYear[team][1]
-		writer.writerow([school, year, prevWinScore, winScore])
+		prevScoreDiff = yearToYearDiff[team][0]
+		scoreDiff = yearToYearDiff[team][1]
+		writer.writerow([school, year, prevWinScore, winScore, prevScoreDiff, scoreDiff])
 
